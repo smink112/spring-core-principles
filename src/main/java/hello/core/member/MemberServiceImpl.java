6b39402b24437dda6e62d17f06fcf21a812544c7
seungmin <smink95@naver.com>
2024-07-10 17:48:03 +0900

    :art: Separation of roles and responsibilities

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,20 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /*
+    설계 변경으로 MemberServiceImpl은 MemoryMemberRepository를 의존하지 않는다.
+    단지 MemberRepository 인터페이스만 의존한다.
+    MemberServiceImpl 입장에서 생성자를 통해 어떤 구현 객체가 들어올지(주입될지) 알 수 없다.
+    MemberServiceImpl의 생성자를 통해서 어떤 구현객체를 주입할지는 오직 외부 AppConfig에서 결정된다.
+    MemberServiceImpl은 이제부터 의존관계에 대한 고민은 외부에 맡기고 '실행에만 집중'하면 된다.
+     */
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        // 실행에만 집중!
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,14 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    //    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
-    void join(){
+    void join() {
         // given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
